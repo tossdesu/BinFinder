c7ef57dfbcb3391378b607f009cc82b3744738e1
Toss <tossdesu@gmail.com>
2023-03-13 19:06:00 +0700

    Add CardInfoActivity.kt, modify activity_main.xml and add activity_bin_info.xml

---
+++ app/src/main/java/com/tossdesu/bankcardinfo/CardInfoActivity.kt
@@ -0,0 +1,11 @@
+package com.tossdesu.bankcardinfo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CardInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bin_info)
+    }
+}
